f82853a3c2db396f1afc36eab8e637663278014e
ganzhilin <835821280@qq.com>
2020-05-31 22:15:46 +0800

    FunnyTitle.js

---
+++ FunnyTitle.js
@@ -0,0 +1,16 @@
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = (¬‿¬) 我藏好了哦~;
+        clearTimeout(titleTime);
+    }
+    else {
+        $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = \﻿ (•◡•) /被你发现了~' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});

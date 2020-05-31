f9d7576cb705fc9bfb9156d7b91f63e8db3e818b
ganzhilin <835821280@qq.com>
2020-05-31 22:43:06 +0800

    FunnyTitle.js

--- FunnyTitle.js
+++ FunnyTitle.js
@@ -1,16 +1,20 @@
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/ganzhilin/ganzhilin.github.io@master/favicon.ico");
-        document.title = (¬‿¬) 我藏好了哦~;
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', "/favicon.ico");
-        document.title = \﻿ (•◡•) /被你发现了~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
+$(window).load(function () {
+//整合页面欺骗特效 window.onload有冲突
+    var OriginTitile = document.title;
+    var titleTime;
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/ganzhilin/ganzhilin.github.io@master/favicon.ico");
+            $('[rel="shortcut icon"]').attr('href', "../../images/failure.png");
+            document.title = '(つェ⊂) 我藏好了哦~ ';
+            clearTimeout(titleTime);
+        } else {
+            $('[rel="icon"]').attr('href', "https://cdn.jsdelivr.net/gh/ganzhilin/ganzhilin.github.io@master/favicon.ico");
+            $('[rel="shortcut icon"]').attr('href', "../../images/favicon.png");
+            document.title = 'o(^▽^)o 被你发现啦~ ';
+            titleTime = setTimeout(function () {
+                document.title = OriginTitile;
+            }, 2000);
+        }
+    });
 });
